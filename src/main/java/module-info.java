5f27a7dcfafaba9d9c7c9d9420017c04694d5a91
r0nz-29 <gs2019048@sgsitsindore.in>
2022-05-28 15:32:24 +0530

    removed unnecessary deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
 module com.raunits.noto {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.web;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.raunits.noto to javafx.fxml;
     exports com.raunits.noto;
 }
